b6ef25303f6fa4f6dad1be7a5911cbf3e5c0db47
Roma Volkov <Drakyla60@gmail.com>
2025-11-28 13:44:29 +0200

    require traefik

--- api/routes/api.php
+++ api/routes/api.php
@@ -8,6 +8,7 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get('/test', function (Request $request) {
+    $f= 'wqed';
     return [
         'test' => 'test',
     ];
